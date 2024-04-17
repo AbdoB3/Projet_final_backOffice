375cd40e3e82734dbfeed60e7b58af5b2ae736a6
AbdoB3 <R139591066@taalim.ma>
2024-04-17 12:08:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+function App() {
+
+  return (
+    <>
+    <DatePicker/>
+      <h1 className=" underline font-bold text-center">
+        Hello world
+      </h1>
+    </>
+  )
+}
+
+export default App
